0bc2b538fe5f1d98e522f14dbd49846e272ba410
Jaemin Jung <james940522@gmail.com>
2026-07-03 18:10:49 +0900

    Fix: etc

--- src/domains/landing/model/branches.ts
+++ src/domains/landing/model/branches.ts
@@ -4,11 +4,14 @@ export type Branch = {
 };
 
 export const branches: Branch[] = [
-  { name: '영종도점', status: '오픈 완료' },
-  { name: '송탄점', status: '오픈 완료' },
-  { name: '사하점', status: '오픈 완료' },
-  { name: '가정점', status: '오픈 완료' },
-  { name: '수원점', status: '오픈 완료' },
   { name: '마포점', status: '오픈 완료' },
   { name: '노원점', status: '오픈 완료' },
+  { name: '가정점', status: '오픈 완료' },
+  { name: '영종도점', status: '오픈 완료' },
+  { name: '송탄점', status: '오픈 완료' },
+  { name: '쌍용점', status: '오픈 완료' },
+  { name: '양천목동점', status: '오픈 완료' },
+  { name: '진주점', status: '오픈 완료' },
+  { name: '사하점', status: '오픈 완료' },
+  { name: '수원점', status: '오픈 완료' },
 ];
